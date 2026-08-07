841aff571b338028d3160fe8b7a8b1d07356a7f1
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-07 09:53:31 +0700

    feat: initialize full protection settings workspace

---
+++ src/Arvrel.Desktop/Controls/ProtectionSettingsWorkspace.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Arvrel.Desktop.Controls;
+
+public sealed partial class ProtectionSettingsWorkspace : UserControl
+{
+    public ProtectionSettingsWorkspace()
+    {
+        InitializeComponent();
+    }
+}
